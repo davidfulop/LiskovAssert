76b7d3a182f14d999642c165de346ee553eadb32
David Fulop <david.fulop@7digital.com>
2018-04-15 17:43:16 +0200

    Add method that gets derived classes.

--- src/LiskovAssertions/LiskovAssert.cs
+++ src/LiskovAssertions/LiskovAssert.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace LiskovAssertions
 {
     public class LiskovAssert
     {
+        public Type[] GetAllSubclassesOf<T>() where T : class
+        {
+            var asm = Assembly.GetAssembly(typeof(T));
+            return asm.GetTypes().Where(t => t.IsSubclassOf(typeof(T))).ToArray();
+        }
     }
 }
--- tests/LiskovAssertions.UnitTests/LiskovAssertTests.cs
+++ tests/LiskovAssertions.UnitTests/LiskovAssertTests.cs
@@ -1,13 +1,23 @@
 using NUnit.Framework;
+using System;
+using LiskovAssertions;
 
 namespace LiskovAssertions.UnitTests
 {
+    class A { }
+    class A1 : A { }
+    class A2 : A { }
+
     public class LiskovAssertTests
     {
+        Type[] _ADerived = new Type[] { typeof(A1), typeof(A2) };
+        
         [Test]
-        public void TestMethod1()
+        public void GetAllSubclassesOf_Gets_All_Subclasses_from_same_assembly()
         {
-            Assert.Pass();
+            LiskovAssert subject = new LiskovAssert();
+            var types = subject.GetAllSubclassesOf<A>();
+            CollectionAssert.AreEquivalent(_ADerived, types);
         }
     }
 }

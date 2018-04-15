9c3c25afe75e5e310f4fc519a907e0f69a26a514
David Fulop <david.fulop@7digital.com>
2018-04-15 17:33:36 +0200

    Add initial project structure.

---
+++ src/LiskovAssertions/LiskovAssert.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LiskovAssertions
+{
+    public class LiskovAssert
+    {
+    }
+}
---
+++ tests/LiskovAssertions.UnitTests/LiskovAssertTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace LiskovAssertions.UnitTests
+{
+    public class LiskovAssertTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass();
+        }
+    }
+}

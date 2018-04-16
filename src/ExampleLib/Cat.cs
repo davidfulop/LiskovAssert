6ae1ebbc4b0767db94c14b975f965fc9c441cb68
davidfulop <28536613+davidfulop@users.noreply.github.com>
2018-04-16 20:03:52 +0200

    Add example library project.

---
+++ src/ExampleLib/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExampleLib
+{
+    //A regular house cat
+    public class Cat
+    {
+        public virtual string Feed(Food food)
+        {
+            return null;
+        }
+    }
+    
+    public enum Food { HumanFood, DryCatFood, CannedCatFood }
+}
---
+++ tests/ExampleLib.UnitTests/CatTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using LiskovAssertions;
+using ExampleLib;
+
+namespace ExampleLib.UnitTests
+{
+    public class CatTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass();
+        }
+    }
+}
